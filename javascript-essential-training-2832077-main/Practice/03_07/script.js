dbf0c0cbbbc737a7ba90acf6c1b79620452f1387
TeowJunWei <Gunzxs2011@gmail.com>
2022-12-03 22:47:42 +0800

    new

--- javascript-essential-training-2832077-main/Practice/03_07/script.js
+++ javascript-essential-training-2832077-main/Practice/03_07/script.js
@@ -7,3 +7,13 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const backpack = {
+    name: "Everday Backpack",
+    volume: 30,
+    color: "grey",
+    pockeyNum: 15,
+    strapLenght:{
+    left:26,
+    right:26,
+    },
+};
