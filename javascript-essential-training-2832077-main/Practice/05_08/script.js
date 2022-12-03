313b30a4ceba03c8d9072555e886cda18c8f2e18
TeowJunWei <Gunzxs2011@gmail.com>
2022-12-04 00:44:18 +0800

    ss

--- javascript-essential-training-2832077-main/Practice/05_08/script.js
+++ javascript-essential-training-2832077-main/Practice/05_08/script.js
@@ -3,3 +3,6 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+document.querySelector("main li:first-child").classList.toggle("new-class")
+
